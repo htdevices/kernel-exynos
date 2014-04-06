5a75d7d54e637c473eb51164f1b1617fdb8cbdb6
Adrian Alonso <aalonso@htdevices.com>
2014-04-06 16:01:26 -0500

    board-exshields: define pmic macros and revision id
    * Define pmic macros and revision id
    * Inline functions for
      get board revision id
      get regutalor/pmic revision id
    
    Signed-off-by: Adrian Alonso <aalonso@htdevices.com>

---
+++ arch/arm/mach-exynos/board-exshields.h
@@ -0,0 +1,46 @@
+/*
+ * Derived from
+ * arch/arm/mach-exynos/board-smdk5250.h
+ * 
+ * Copyright (c) 2014 High Technology Devices LLC.
+ * Copyright (c) 2012 Samsung Electronics Co., Ltd.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef __MACH_EXYNOS_BOARD_EXSHIELDS_H
+#define __MACH_EXYNOS_BOARD_EXSHIELDS_H
+
+#include <asm/system_info.h>
+
+#define EXSHIELDS_REV_0_0		0x0
+#define EXSHIELDS_REV_0_1		0x1
+#define EXSHIELDS_REV_0_2		0x2
+#define EXSHIELDS_REV_MASK		0xf
+
+#define EXSHIELDS_REGULATOR_S5M8767	0x2
+#define EXSHIELDS_REGULATOR_SHIFT	16
+#define EXSHIELDS_REGULATOR_MASK	0xf
+
+static inline int get_exshields_rev(void)
+{
+	return system_rev & EXSHIELDS_REV_MASK;
+}
+
+static inline int get_exshields_regulator(void)
+{
+	return (system_rev >> EXSHIELDS_REGULATOR_SHIFT) \
+		& EXSHIELDS_REGULATOR_MASK;
+}
+
+void exynos5_exshields_power_init(void);
+
+#endif
